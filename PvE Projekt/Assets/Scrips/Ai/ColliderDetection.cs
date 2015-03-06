3aadef4fa48f0b763d14203dc746a4cf3c7600f9
Godric2010 <godric2010@live.de>
2015-03-06 13:41:31 +0100

    Added script for the Enemys which checks if the player is in range of vision or of hearing.

---
+++ PvE Projekt/Assets/Scrips/Ai/ColliderDetection.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderDetection : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        //Debug.Log(LayerMask.LayerToName(other.gameObject.layer));
+        //if(other.gameObject.layer == LayerMask.NameToLayer("see"))
+        //{
+        //    if (other.transform.gameObject.layer == LayerMask.NameToLayer("Player"))
+        //        Debug.Log("Saw you, Player!");
+        //}
+        
+        //if(other.gameObject.layer == LayerMask.NameToLayer("hear"))
+        //{
+        //     if (other.transform.gameObject.layer == LayerMask.NameToLayer("Player")) 
+        //        Debug.Log("Heard you, Player!");
+        //}
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+
+            if (gameObject.layer == LayerMask.NameToLayer("see"))
+                Debug.Log("Saw you, Player!");
+            if (gameObject.layer == LayerMask.NameToLayer("hear"))
+                Debug.Log("Heard you, Player!");
+        }
+       
+    }
+}
