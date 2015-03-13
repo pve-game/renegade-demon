9b08ee8de5482bc5ad29b6c725622a14c9fb79f0
rmwettig <rmwettig@users.noreply.github.com>
2015-03-13 10:22:27 +0100

    add DamageOnHit and ObjectPool stub

---
+++ PvE Projekt/Assets/Scrips/Combat/DamageOnHit.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Applies damage to the hit object
+/// </summary>
+/// <remarks>
+/// Author: Martin Wettig
+/// </remarks>
+public class DamageOnHit : MonoBehaviour 
+{
+    [SerializeField]
+    private int damage = 0;
+    public int Damage { get { return damage; } set { damage = Mathf.Max(value, 0); } }
+
+    /// <summary>
+    /// Determine which layer should be checked for collisions
+    /// </summary>
+    private int collisionLayer = 0;
+    public string collisionLayerName = "Default";
+
+    /// <summary>
+    /// Notify registered classes if a hit occured
+    /// </summary>
+    public delegate void HitOccured();
+    public HitOccured onHitOccured;
+    public HitOccured OnHitOccured { get { return onHitOccured; } }
+
+    public void Awake()
+    {
+        collisionLayer = LayerMask.NameToLayer(collisionLayerName);
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        GameObject other = col.gameObject;
+        if (other.layer == collisionLayer)
+        {
+            Health h = other.GetComponent<Health>();
+            h.addHealth(-damage);
+            if (onHitOccured != null)
+                onHitOccured();
+        }
+    }
+}
---
+++ PvE Projekt/Assets/Scrips/Management/ObjectPool.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Facilitates the re-use of already instantiated objects
+/// </summary>
+/// <remarks>
+/// Author: Martin Wettig
+/// </remarks>
+public class ObjectPool
+{
+    private GameObject[] pool;
+    private int capacity = 1;
+    private int current = 0;
+    public ObjectPool(int capacity)
+    {
+        pool = new GameObject[capacity];
+        this.capacity = capacity;
+    }
+
+    /// <summary>
+    /// gets the next available object
+    /// </summary>
+    /// <returns>a usable object</returns>
+    public GameObject GetObject()
+    {
+        GameObject go = null;
+        for (int i = 0; i < pool.Length; i++)
+        {
+            if (!pool[i].activeInHierarchy)
+            {
+                go = pool[i];
+                Debug.Log(go);
+                break;
+            }
+        }
+        return go;
+    }
+
+    public void AddObject(GameObject go)
+    {
+        if(pool.Length != capacity)
+        {
+            pool[current] = go;
+            ++current;
+        }
+    }
+}
